31d87526a5948c789273a00f2288c8808a54ad1b
Michael Shimniok <shimniok@gmail.com>
2014-06-16 21:59:07 -0600

    Fixed bugs with Serial LCD output

--- DataBus/UI/Display/Display.cpp
+++ DataBus/UI/Display/Display.cpp
@@ -4,69 +4,89 @@
 #include "Display.h"
 
 // TODO 3: move to Sparkfun class. This is for the Sparkfun module
-#define DISPLAY_CLEAR     0x01
-#define DISPLAY_SET_POS   0x08
+
+#define WIDTH 16
+
+#define LCD_COMMAND				0x7c
+#define LCD_EXTENDED			0xfe
+#define LCD_BACKLIGHT           0x80
+#define LCD_CLEAR		        0x01
+#define LCD_POS                 0x80
+#define LCD_HOME       		    0x02
 
 Display::Display(void):
 	_lcd(LCDTX, LCDRX)
 {
-	_lcd.baud(9600);
-	// nothin to do
+	// nothing to do yet
 }
 
 void Display::init()
 {
-//	_lcd.puts("test\n"); // hopefully force 115200 on powerup
-//    _lcd.putc(0x7c);
-//    _lcd.putc(0x01);
-//    wait(0.3);
+	_lcd.baud(9600);
+	// Set brightness
+	for (int i=0; i < 2000; i++) {
+		_lcd.putc(0x12);
+	}
+	wait(0.5);
+	// 9600 baud
+	_lcd.putc(LCD_COMMAND);
+	_lcd.putc(0x0D);
+	wait(0.5);
+	// Set backlight
+//	_lcd.putc(LCD_COMMAND);
+//	_lcd.putc(150);
+	// Clear screen
+	_lcd.putc(LCD_EXTENDED);
+	_lcd.putc(LCD_CLEAR);
+	wait(0.5);
 }
 
 void Display::status(const char *st)
 {
-//	int pad;
-//	char *s = (char *) st;
-//	// TODO 3: parameterize screen width
-//	for (pad=15; pad > 0; pad--) {
-//		if (*s++ == '\0') break;
-//	}
-//    lcd.pos(0,1);
-//    lcd.print(st);
-//    while (pad--) {
-//    	lcd.print(" ");
-//    }
+	int pad;
+    char *s = (char *) st;
+// TODO 3: parameterize screen width
+	for (pad=WIDTH; pad > 0; pad--) {
+		if (*s++ == '\0') break;
+	}
+	_lcd.putc(0xfe);
+	_lcd.putc((1<<7)|64); // line 2
+	_lcd.puts(st);
+    while (pad--) {
+    	_lcd.putc(' ');
+    }
 }
 
 void Display::menu(const char *itemName)
 {
-//	int pad;
-//	char *s = (char *) itemName;
-//	for (pad=20; pad > 0; pad--) {
-//		if (*s++ == '\0') break;
-//	}
-//    lcd.pos(0,0);
-//    lcd.print("< ");
-//    lcd.print(itemName);
-//    lcd.print(" >");
-//    pad -= 4; // account for "< " and " >"
-//    while (pad--) {
-//    	lcd.print(" ");
-//    }
+	int pad;
+	char *s = (char *) itemName;
+	for (pad=20; pad > 0; pad--) {
+		if (*s++ == '\0') break;
+	}
+	_lcd.putc(0xfe);
+	_lcd.putc((1<<7)); // line 0, col 3
+	_lcd.puts("< ");
+	_lcd.puts(itemName);
+	_lcd.puts(" >");
+    pad -= 4; // account for "< " and " >"
+    while (pad--) {
+    	_lcd.putc(' ');
+    }
 }
 
 void Display::select(const char *itemName)
 {
-//    lcd.pos(0,0);
-//    lcd.print(">>");
-//	lcd.print(itemName);
+	_lcd.putc(0xfe);
+	_lcd.putc((1<<7));
+	_lcd.puts(">>");
+	_lcd.puts(itemName);
 }
 
 // display gauge at a given position (slot) along the bottom
 void Display::gauge(int slot)
 {
 }
-
-#define WIDTH 22
 
 void Display::update(SystemState *state) {
 #if 0
